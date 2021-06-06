7b6b0c0980975571b11c3e37015d104cedc37dde
sra1kumar-NULL <sravansandeep163@gmail.com>
2021-06-06 22:16:26 +0530

    initial commit

---
+++ coronavirus-tracker/src/test/java/com/sam/corleone/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.corleone.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
